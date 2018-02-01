950b7ae09132b4bf4c12b46e3b5890b774233ebf
Renato Groffe <renato.groffe@yahoo.com.br>
2018-01-31 23:58:54 -0200

    Selenium Web Driver + .NET Core 2.0 + .NET Standard 2.0 + xUnit
    Selenium Web Driver + .NET Core 2.0 + .NET Standard 2.0 + xUnit

---
+++ ExemploSelenium/Selenium.Utils/WebDriverFactory.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.Utils
+{
+    public static class WebDriverFactory
+    {
+        public static IWebDriver CreateWebDriver(
+            Browser browser, string pathDriver)
+        {
+            IWebDriver webDriver = null;
+
+            switch (browser)
+            {
+                case Browser.Firefox:
+                    FirefoxDriverService service =
+                        FirefoxDriverService.CreateDefaultService(pathDriver);
+
+                    /*FirefoxOptions optionsFF = new FirefoxOptions();
+                    optionsFF.AddArgument("--headless");
+
+                    webDriver = new FirefoxDriver(
+                        service, optionsFF, TimeSpan.FromSeconds(30));*/
+                    webDriver = new FirefoxDriver(service);
+
+                    break;
+                case Browser.Chrome:
+                    //ChromeOptions options = new ChromeOptions();
+                    //options.AddArgument("--headless");
+
+                    //webDriver = new ChromeDriver(pathDriver, options);
+                    webDriver = new ChromeDriver(pathDriver);
+
+                    break;
+            }
+
+            return webDriver;
+        }
+    }
+}
